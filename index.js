f13fb473ed10bdd8627ab205f42971953a6ac023
yijun.liu <francislyj@gmail.com>
2021-11-17 21:48:19 +0800

    feat

---
+++ index.js
@@ -0,0 +1,9 @@
+// Copyright 2021 Yijun Liu <francislyj@gmail.com>. All rights reserved.
+// Use of this source code is governed by a MIT style
+// license that can be found in the LICENSE file.
+
+const { FiniteStateMachine } = require('./lib/finite_state_machine');
+
+module.exports = {
+  FiniteStateMachine
+}
